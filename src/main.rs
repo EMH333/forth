b351561c9df745419f278dfd95cfe54c1f8b29ee
Ethan Hampton <etho@ethohampton.com>
2023-07-16 20:53:56 -0700

    Add ."

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,91 @@
 use std::collections::HashMap;
-use std::io;
-use std::io::BufRead;
+use std::{env, fmt, io};
+use std::fs::{File, read_to_string};
+use std::io::{BufRead, BufReader, Lines};
+use std::path::Path;
+use std::string::ToString;
 
-fn main() {
-    println!("Hello, world!");
+fn blank_ok() -> Result<String, String> {
+    Ok("".to_string())
+}
 
+fn underflow_err() -> Result<String, String> {
+    Err("Stack Underflow".to_string())
+}
+
+fn read_lines<P>(filename: P) -> io::Result<io::BufReader<File>>
+    where P: AsRef<Path>, {
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file))
+}
+
+type Error = Box<dyn std::error::Error + Send + Sync + 'static>;
+
+struct State {
+    defined_words: HashMap<String, String>,
+    print_quote: bool,
+}
+
+fn main() -> Result<(), Error> {
     let mut stack = Vec::new();
-    let mut defined_words : HashMap<String, String> = HashMap::new();
+    let mut state: State;
+    state = State { defined_words: Default::default(), print_quote: false };
 
     // read in words from std (or file eventually) and evaluate
-    let stdin = io::stdin();
-    for line in stdin.lock().lines() {
+    let path = match std::env::args_os().nth(1) {
+        Some(path) => std::path::PathBuf::from(path),
+        None => {
+            return Err(Error::from("Usage: cmd <path>"));
+        }
+    };
+    let mut input: Box<dyn std::io::BufRead + 'static> = if path.as_os_str() == "-" {
+        Box::new(std::io::stdin().lock())
+    } else {
+        match std::fs::File::open(&path) {
+            Ok(file) => Box::new(BufReader::new(file)),
+            Err(err) => {
+                let msg = format!("{}: {}", path.display(), err);
+                return Err(Error::from(msg));
+            }
+        }
+    };
+
+    for line in input.lines() {
         for word in line.unwrap_or("".to_string()).split(' ') {
-            let result = run_word(&mut stack, &mut defined_words, word);
+            let result = run_word(&mut stack, &mut state, word);
             if result.is_ok() {
-                println!("{}", result.unwrap())
+                let out = result.unwrap();
+                if !out.is_empty() {
+                    println!("{}", out)
+                }
             } else {
-                println!("{}", result.unwrap_err())
+                println!("{}", result.unwrap_err());
+                return Err(Error::from("Unrecognized Word"));
             }
         }
     }
+    return Ok(());
 }
 
-fn run_word(stack: &mut Vec<i64>, defined_words : &mut HashMap<String, String>, word: &str) -> Result<String, String> {
+fn run_word(stack: &mut Vec<i64>, state: &mut State, word: &str) -> Result<String, String> {
     let int = word.parse::<i64>();
     if int.is_ok() {
         stack.push(int.unwrap());
         return Ok("".to_string());
     }
 
+    if state.print_quote && word != "\"" {
+        print!("{}", word);
+        return blank_ok()
+    }
+    if state.print_quote && word == "\"" {
+        println!();
+        state.print_quote = false;
+        return blank_ok()
+    }
+
     // must be an actual word
-    return match word {
+    return match word.to_lowercase().as_str() {
         "+" => {
             if stack.len() < 2 {
                 stack.clear()
@@ -38,19 +93,32 @@ fn run_word(stack: &mut Vec<i64>, defined_words : &mut HashMap<String, String>, 
             let one = stack.pop().unwrap();
             let two = stack.pop().unwrap();
             stack.push(one + two);
-            Ok("".to_string())
+            blank_ok()
         }
         "." => {
             let result = stack.pop();
             if result.is_some() {
                 println!("{}", result.unwrap());
-                return Ok("".to_string())
+                blank_ok()
             } else {
-                Err("Stack Underflow".to_string())
+                underflow_err()
             }
         }
-        _ => {
-            Err("Unrecognized word".to_string() + &word)
+        "reset" => {
+            //don't do a ton at this point, will be useful later
+            stack.clear();
+            blank_ok()
         }
-    }
+        ".\"" => {
+            state.print_quote = true;
+            blank_ok()
+        }
+        _ => {
+            if state.defined_words.contains_key(word) {
+                //TODO defined stuff
+            }
+
+            Err("Unrecognized word ".to_string() + &word)
+        }
+    };
 }
---
+++ test.forth
@@ -0,0 +1,44 @@
+." Reset... " RESET
+." Check comments... " \ Yes, we support the new-style comments :-)
+." Computing simple addition of 3 + 4... " 3 4 + .
+." Is 1 = 2 ?... " 1 2 = .
+." Is 1 > 2 ?... " 1 2 > .
+." Is 1 < 2 ?... " 1 2 < .
+." Define pi at double-word precision... " : pi 355 113 */ ;
+." Use definition to compute 10K times PI... " 10000 pi .
+." Check: 23 mod 7... " 23 7 MOD .
+." Defining 1st level function1... " : x2 2 * ;
+." Defining 1st level function2... " : p4 4 + ;
+." 2nd level word using both - must print 24... " 10 x2 p4 .
+." Defining a variable with value 123... " 123 variable ot3
+." Printing variable's value... " ot3 @ .
+." Defining The Constant (TM)... " 42 constant lifeUniverse
+." Printing The Constant (TM)... " lifeUniverse .
+." Setting the variable to The Constant (TM)... " lifeUniverse ot3 !
+." Printing variable's value... " ot3 @ .
+." Setting the variable to hex 0x11... " $11 ot3 !
+." Printing variable's value... " ot3 @ .
+." Defining helper... " : p5 5 U.R . ;
+." Defining 3 times loop... " : x3lp 3 0 DO I p5 LOOP ;
+." Calling loop... " x3lp
+." Defining loop calling loop 2 times... " : x6lp 2 0 DO x3lp LOOP ;
+." Nested-looping 2x3 times... " x6lp
+." Inline: " : m 3 1 DO 3 1 DO CR J p5 I p5 ." = " J I * p5 LOOP LOOP ;
+." Use inline loops with two indexes... " m
+." Make multiples of 7 via DUP... " : m7s 10 0 DO DUP I * . LOOP DROP ;
+." Print them and DROP the 7... " 7 m7s
+." Reset... " RESET
+\ Time for Turing completeness...
+." Let's do Fizz-Buzz! " \ Turing Completeness check...
+\ fizz ( n -- 0_or_1 n )
+." Define fizz... " : fizz DUP 3 MOD 0 = IF ." fizz " 1 ELSE 0 THEN SWAP ;
+\ buzz ( n -- 0_or_1 n )
+." Define buzz... " : buzz DUP 5 MOD 0 = IF ." buzz " 1 ELSE 0 THEN SWAP ;
+\ emitNum ( 0_or_1 0_or_1 n -- )
+." Define emitNum... " : emitNum ROT ROT + 0 = if . ELSE DROP THEN ;
+\ mainloop ( n -- )
+." Define mainloop... " : mainloop fizz buzz emitNum ;
+\ fb ( -- )
+." Define fizzbuzz... " : fb 37 1 DO I mainloop LOOP ;
+." Run it! " fb
+." Report memory usage... " .S
